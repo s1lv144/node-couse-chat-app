d061c3321a099213063163abe849e31416b8996d
s1lv144 <pngun403@gmail.com>
2018-03-31 13:22:11 -0300

    'scrolling'

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
+  function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +32,7 @@ socket.on('newMessage', function (message) {
 	   
    });
    jQuery('#messages').append(html);
+   scrollToBottom();
   /*var formattedTime=moment(message.CreateAt).format('h:mm a');
   var li = jQuery('<li></li>');
   li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -36,6 +51,7 @@ socket.on('newLocationMessage', function (message) {
 	   
    });
    jQuery('#messages').append(html);
+    scrollToBottom();
   /*var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
