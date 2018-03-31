f7ef8fa73af095b4187582fbd1c3a532ee9395cc
s1lv144 <pngun403@gmail.com>
2018-03-31 16:50:28 -0300

    'listau'

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,13 +15,31 @@ var socket = io();
   }
 }
 socket.on('connect', function () {
-  console.log('Connected to server');
+  //console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+  socket.emit('join',params,function(err){
+	   if (err){
+		 alert(err);  
+		 window.location.href ='/';  
+	   }else{
+		  console.log('no errro'); 
+	   }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+//mostrando la lista de usuarios
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
 
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
 socket.on('newMessage', function (message) {
 	 var formattedTime=moment(message.CreateAt).format('h:mm a');
    var template =jQuery('#message-template').html();
